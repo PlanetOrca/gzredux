0275d89f188e44184e9a76dfde925ebcd14f56aa
PlanetOrca <73614166+PlanetOrca@users.noreply.github.com>
2021-03-02 17:06:33 -0600

    Update host.bat

--- Bats/host.bat
+++ Bats/host.bat
@@ -7,6 +7,12 @@ set /p a="Difficulty?(0-4, also affects item placement in deathmatch)"
 set b=%b% -skill %a%
 set /p a="Which level? (Format is 'Episode Mission', so 3 8 is E3M8)"
 set b=%b% -warp %a%
+set /p a="Use most recent addons? (y/n)
+if /i %a%==y for /f "delims=" %%x in (ModList\recent.txt) do set c=%%x
+if /i %a%==y echo %c%
+if /i %a%==y goto skipad
 set /p a="Addons"
+:skipad
+if /i %a%==y set a=%c%
 set b=%b% %a%
 ..\Base_Files\gzdoom.exe %b%
